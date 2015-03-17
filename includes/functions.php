89a0bdcd6b6dfc38553e04dfa6aa97b71836c646
Zishan Ahmad <zishanahmad@outlook.com>
2015-03-17 22:58:01 +0530

    Removed unnecessary functions

--- includes/functions.php
+++ includes/functions.php
@@ -22,27 +22,9 @@
 		$row = mysqli_fetch_array($result);
 		return $row['score'];
 	}
-	
-	function update_capital($user_id,$capital) {
-		global $connection;
-		$query = "UPDATE users ";
-		$query .= "SET capital={$capital} ";
-		$query .= "WHERE id={$user_id}";
-		$result = mysqli_query($connection,$query) or die("Cannot update capital<br />" . mysqli_error($connection));
-	}
-	
-	function get_stock_capital($user_id) {
-		global $connection;
-		$capital = 0;
-		$query = "SELECT stock_price ";
-		$query .= "FROM stocks_bought WHERE user_id={$_SESSION['user_id']}";
-		$result = mysqli_query($connection,$query) or die("Cannot get capital from the database<br />" . mysqli_error($connection));
-		while($row = mysqli_fetch_array($result)){
-			$capital += $row['stock_price'];
-		}
-		return $capital;
-	}
-	
+
+
+
 	function update_score($user_id,$amount) {
 		global $connection;
 		$query = "UPDATE players ";
@@ -50,6 +32,6 @@
 		$query .= "WHERE id={$user_id}";
 		$result = mysqli_query($connection,$query) or die("Cannot update capital<br />" . mysqli_error($connection));
 	}
-	
-	
+
+
 ?>
